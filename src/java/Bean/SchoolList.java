d19672c7ed6db5e78f9a95f2379116547f2fed90
DrashtiSabhaya <sabhayadrashti@gmail.com>
2020-10-16 22:12:05 +0530

    all

---
+++ src/java/Bean/SchoolList.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Bean;
+
+/**
+ *
+ * @author Drashti
+ */
+public class SchoolList {
+    private int count;
+    private int criteria;
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public int getCriteria() {
+        return criteria;
+    }
+
+    public void setCriteria(int criteria) {
+        this.criteria = criteria;
+    }
+    
+}
